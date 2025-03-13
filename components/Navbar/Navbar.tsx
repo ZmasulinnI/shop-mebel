6a36881bb503a43cea2bff7fef36de4082ebe4f0
ZmasulinnI <v.poroshin1604@yandex.ru>
2025-03-13 18:50:45 +0300

    creating navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Image from 'next/image'
+import { Heart, Search, ShoppingCart } from 'lucide-react';
 
-const Navbar = ({}) => {
+const Navbar = ({ }) => {
     return (
         <>
-            <div className=''>
-                <Image src={'nav_logo.svg'} width={185} height={41} alt='logo'/>
+            <div className='mx-auto w-fit flex items-center my-7'>
+                <Image src={'nav_logo.svg'} width={185} height={41} alt='logo' className='mr-[266px]'/>
+                <div className='flex gap-18 mr-[231px]'>
+                    <a href="">Home</a>
+                    <a href="">Shop</a>
+                    <a href="">About</a>
+                    <a href="">Contact</a>
+                </div>
+                <div className='flex gap-11'>
+                    <Search size={28}/>
+                    <Heart size={28}/>
+                    <ShoppingCart size={28}/>
+                </div>
             </div>
         </>
     );
